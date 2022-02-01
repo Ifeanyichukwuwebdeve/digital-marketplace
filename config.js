c8789b3f49c36798583b8b170db1cf1d61029483
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2022-02-01 23:37:10 +0100

    Deployed to mumbie testnet

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
-export const nftMarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+export const nftMarketAddress = '0x13f6928e5B43913B358CFB40C3ab111DC1ac213c'
+export const nftAddress = '0xD775B3E8Df1C9ea0eFA37af8E19cF2Bc429d8dE4'
